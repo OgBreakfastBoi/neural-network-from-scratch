995122f2aad3372dc8fe35eaa002070545ebaffe
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-04 09:29:11 +0200

    src/nn __init__

---
+++ src/nn/__init__.py
@@ -0,0 +1,11 @@
+import src.nn.activations
+from src.nn.errors import (
+    ModelNotCompiledError,
+    NoLayersError,
+)
+from src.nn.layers import Dense
+from src.nn.losses import (
+    cross_categorical_entropy_loss,
+    mean_squared_error,
+)
+from src.nn.network import NeuralNetwork
