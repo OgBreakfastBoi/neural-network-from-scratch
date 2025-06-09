155353e1ceb5fe66f1bbc161c5a5bf980a2ee2f9
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-09 18:17:58 +0200

    Modularized losses.py

---
+++ src/nn/losses/__init__.py
@@ -0,0 +1,7 @@
+from src.nn.losses.losses import cross_categorical_entropy_loss
+from src.nn.losses.losses import mean_squared_error
+
+__all__ = [
+    "cross_categorical_entropy_loss",
+    "mean_squared_error"
+]
