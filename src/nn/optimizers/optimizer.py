f8fa8e2edd73ffffe0398448baf64a8696456b52
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-17 19:30:52 +0200

    Implemented optimizers package with 'Adam' and 'SDG' optimizers and an appropriate base class.

---
+++ src/nn/optimizers/optimizer.py
@@ -0,0 +1,24 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
+
+import numpy as np
+
+
+class Optimizer(ABC):
+    def __init__(self, lr: float, name: str, ):
+        self.lr = lr
+        self.name = name
+
+    def __repr__(self):
+        return f"<Optimizer: {self.name}, lr={self.lr}>"
+
+    @abstractmethod
+    def step(
+        self,
+        param: np.ndarray,
+        grad: np.ndarray,
+        param_name: str,
+    ) -> np.ndarray:
+        raise NotImplementedError
