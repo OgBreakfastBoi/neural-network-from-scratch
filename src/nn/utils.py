eccac8b9dca1d938123bad9cdc83af4830ea3b0a
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-17 19:12:42 +0200

    Changed weight and bias init to 'He' and 'constant' respectively.

--- src/nn/utils.py
+++ src/nn/utils.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def initialize_weights(nodes: int, inputs_per_node: int) -> np.ndarray:
-    return np.random.randn(nodes, inputs_per_node)
+    return np.random.normal(0, np.sqrt(2 / inputs_per_node), (inputs_per_node, nodes))
 
 
 def initialize_biases(nodes: int) -> np.array:
-    return np.random.randn(nodes)
+    return np.zeros(nodes)
