df0804741b385778a23485d275dd742a91513b30
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-18 12:01:45 +0200

    Implemented InputLayer base class, Layer built check and backward method

--- src/nn/layers/layer.py
+++ src/nn/layers/layer.py
@@ -5,16 +5,32 @@ from abc import (
 
 import numpy as np
 
+from src.nn.errors import LayerNotBuiltError
+from src.nn.optimizers.optimizer import Optimizer
+
 
 class Layer(ABC):
-    def __call__(self, *args, **kwargs):
-        return self.call(*args, **kwargs)
+    def __init__(self, name: str):
+        self.name = name
+
+        self._idx = -1  # The index of the layer
+        self._built = False
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        if not self._built:
+            raise LayerNotBuiltError()
+
+        return self.call(x)
 
     def __repr__(self):
-        return f"<Layer: {self.__class__.__name__}>"
+        return f"<Layer: {self.name}, index={self._idx}>"
 
     @abstractmethod
     def build(self, *args, **kwargs):
+        """
+        `self._idx` should be set here and `self._built` should
+        be set to True after the build process is complete.
+        """
         raise NotImplementedError
 
     @abstractmethod
@@ -22,5 +38,17 @@ class Layer(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def compute_output_shape(self) -> tuple[int]:
+    def backward(self, grad_input: np.ndarray, optimizer: Optimizer) -> np.ndarray:
         raise NotImplementedError
+
+    @abstractmethod
+    def output_shape(self) -> tuple[int]:
+        raise NotImplementedError
+
+    @property
+    def built(self):
+        return self._built
+
+
+class InputLayer:
+    pass
