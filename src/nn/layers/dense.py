6e4dfbfbcf3fe6195a3e5c4f2dc405d03c560c3f
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-18 12:07:08 +0200

    Implemented backword method and updated build method

--- src/nn/layers/dense.py
+++ src/nn/layers/dense.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-import src.nn.activations as activations
+from src.nn import activations
+from src.nn.errors import LayerNotBuiltError
 from src.nn.layers.layer import Layer
+from src.nn.optimizers.optimizer import Optimizer
 from src.nn.utils import (
     initialize_biases,
     initialize_weights,
@@ -9,10 +11,12 @@ from src.nn.utils import (
 
 
 class Dense(Layer):
-    def __init__(self, units: int, activation: str = None):
-        super().__init__()
+    def __init__(self, units: int, activation: str = None, name = "Dense"):
+        super().__init__(name)
         self.units = units
         self.activation = activations.get(activation)
+        self.prev_input = np.array([])
+        self.prev_output = np.array([])
 
         self._weights = np.array([])
         self._biases = np.array([])
@@ -25,22 +29,40 @@ class Dense(Layer):
     def biases(self):
         return self._biases
 
-    def build(self, inputs_per_node: int):
+    def build(self, inputs_per_node: int, index: int):
+        self._idx = index
         self._weights = initialize_weights(self.units, inputs_per_node)
         self._biases = initialize_biases(self.units)
+        self._built = True
 
     def call(self, x: np.ndarray) -> np.ndarray:
-        """
-        Args:
-            x: A flattened NumPy array of input data with shape `(len(x),)`
+        self.prev_input = x
+        x = x @ self._weights + self._biases
+        self.prev_output = self.activation(x)
+        return self.prev_output
 
-        Returns:
-            A NumPy array containing the outputs of all the neurons in this layer after
-            the forward pass process with the shape `(len(x),)`
-        """
+    def backward(self, grad_input: np.ndarray, optimizer: Optimizer) -> np.ndarray:
+        if not self._built:
+            raise LayerNotBuiltError()
 
-        x = np.sum(x * self.weights, axis=1) + self.biases
-        return self.activation(x)
+        activation_deriv = self.activation.deriv(self.prev_output)
+        if activation_deriv.ndim == 3:
+            delta = np.einsum("bij,bi->bj", activation_deriv, grad_input)
+        else:
+            delta = grad_input * activation_deriv
 
-    def compute_output_shape(self) -> tuple[int]:
+        self._weights = optimizer.step(
+            self._weights,
+            self.prev_input.T @ delta,
+            f"weights{self._idx}"
+        )
+        self._biases = optimizer.step(
+            self._biases,
+            np.sum(delta, axis=0),
+            f"biases{self._idx}"
+        )
+
+        return delta @ self._weights.T
+
+    def output_shape(self) -> tuple[int]:
         return (self.units,)
