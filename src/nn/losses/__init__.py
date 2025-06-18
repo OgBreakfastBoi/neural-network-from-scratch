15dbfa28927f24d72fb54610f56c3ae1d77c74d7
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-18 11:51:48 +0200

    Removed instances for direct access to loss functions classes

--- src/nn/losses/__init__.py
+++ src/nn/losses/__init__.py
@@ -1,20 +1,20 @@
 """
 This package provides loss functions for the neural network.
 
-It exposes the base ``LossFunction`` class, instances of common loss functions,
+It exposes the base ``LossFunction`` class, classes of common loss functions,
 and a ``get`` function to retrieve loss functions by name.
 """
 
 from src.nn.losses.loss import LossFunction
-from src.nn.losses.registry import (
-    categorical_cross_entropy,
-    get,
-    mean_squared_error,
+from src.nn.losses.losses import (
+    CategoricalCrossEntropy,
+    MeanSquaredError,
 )
+from src.nn.losses.registry import get
 
 __all__ = [
     "LossFunction",
-    "categorical_cross_entropy",
-    "mean_squared_error",
+    "CategoricalCrossEntropy",
+    "MeanSquaredError",
     "get"
 ]
