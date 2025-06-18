330bdea2a0de776229e0be9588da81fdd59ebea5
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-18 12:05:51 +0200

    Made Flatten inherit InputLayer and implemented build and backword methods

--- src/nn/layers/flatten.py
+++ src/nn/layers/flatten.py
@@ -2,19 +2,25 @@ import math
 
 import numpy as np
 
-from src.nn.layers.layer import Layer
+from src.nn.errors import LayerNotBuiltError
+from src.nn.layers.layer import (
+    InputLayer,
+    Layer,
+)
+from src.nn.optimizers.optimizer import Optimizer
 
 
-class Flatten(Layer):
-    def __init__(self, input_shape: tuple[int, int]):
-        super().__init__()
+class Flatten(Layer, InputLayer):
+    def __init__(self, input_shape: tuple[int, int], name = "Flatten"):
+        super().__init__(name)
         self.input_shape = input_shape
 
-    def build(self, *args, **kwargs):
-        pass
+    def build(self, index: int):
+        self._idx = index
+        self._built = True
 
     def call(self, x: np.ndarray) -> np.ndarray:
-        output_shape = self.compute_output_shape()[-1]
+        output_shape = self.output_shape()[-1]
 
         if x.ndim == 1 and x.shape[0] != output_shape:
             raise ValueError(
@@ -41,5 +47,10 @@ class Flatten(Layer):
 
         return x
 
-    def compute_output_shape(self) -> tuple[int]:
+    def backward(self, grad_input: np.ndarray, optimizer: Optimizer) -> np.ndarray:
+        if not self._built:
+            raise LayerNotBuiltError()
+        return np.array([])
+
+    def output_shape(self) -> tuple[int]:
         return (math.prod(self.input_shape),)
