edf9ad23947201daf5b2df1cf84d23ccc89e4156
OgBreakfastBoi <forbangk31@gmail.com>
2025-06-23 19:41:55 +0200

    Update layers modules init to include InputLayer class and from_config method

--- src/nn/layers/__init__.py
+++ src/nn/layers/__init__.py
@@ -1,9 +1,15 @@
 from src.nn.layers.dense import Dense
 from src.nn.layers.flatten import Flatten
-from src.nn.layers.layer import Layer
+from src.nn.layers.layer import (
+    InputLayer,
+    Layer,
+)
+from src.nn.layers.registry import from_config
 
 __all__ = [
     "Dense",
     "Flatten",
-    "Layer"
+    "Layer",
+    "InputLayer",
+    "from_config"
 ]
